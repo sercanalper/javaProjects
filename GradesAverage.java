c3f69625d23153e2f2faf4d1b2e7ea7933b120c1
Sercan Alper Şahin <sercanalper92@gmail.com>
2022-11-08 12:19:28 +0300

    My first Java project has been added.

---
+++ GradesAverage.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class GradesAverage {
+    public static void main(String[] args) {
+
+        int math, science, music, literature, sports;
+
+        try (Scanner gradeinputs = new Scanner(System.in)) {
+            System.out.println("Math Grade: ");
+            math = gradeinputs.nextInt();
+
+            System.out.println("Science Grade: ");
+            science = gradeinputs.nextInt();
+
+            System.out.println("Music Grade: ");
+            music = gradeinputs.nextInt();
+
+            System.out.println("Literature Grade: ");
+            literature = gradeinputs.nextInt();
+
+            System.out.println("Sports Grade: ");
+            sports = gradeinputs.nextInt();
+        }
+
+        int total = math + science + music + literature + sports;
+        double takeaverage = total / 6;
+        System.out.println("Your average is: " + takeaverage);
+        
+        String result;
+        result = (takeaverage > 60) ? "You've passed !" : "You should take some classes.";
+        System.out.println(result);
+    }
+}
